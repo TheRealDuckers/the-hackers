3cfb0d20196410f45a8a2b48660ab40eedb2c613
Duckers <198241505+TheRealDuckers@users.noreply.github.com>
2025-12-30 11:52:25 +0000

    Update server.js

--- platform/server.js
+++ platform/server.js
@@ -131,7 +131,7 @@ res.redirect("/");
 // ----------------------
 
 app.get("/", requireAuth, (req, res) => {
-  let html = fs.readFileSync(__dirname + "/home.html", "utf8");
+  let html = fs.readFileSync(__dirname + "/index.html", "utf8");
   html = html.replace("{{NAME}}", req.session.user.identity.first_name);
   res.send(html);
 });
